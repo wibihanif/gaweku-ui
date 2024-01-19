dcda0429e3b5bfdffe83e0ae9a006b066d1a748e
Wibi Hanif Wibowo <wibihanifwibowo@gmail.com>
2024-01-19 11:38:30 +0700

    fix ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,8 @@ import { LoginForm, LoginFormHeader } from "../features/login";
 
 export default function Login() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div>
+    <div className="flex items-center justify-center h-screen overflow-hidden">
+      <div className="max-h-screen overflow-y-auto">
         <LoginFormHeader />
         <LoginForm />
       </div>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,8 +2,8 @@ import { RegisterForm, RegisterFormHeader } from "../features/register";
 
 export default function Register() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div>
+    <div className="flex items-center justify-center h-screen overflow-hidden">
+      <div className="max-h-screen overflow-y-auto">
         <RegisterFormHeader />
         <RegisterForm />
       </div>
